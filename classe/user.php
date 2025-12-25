977a29addc534a9baaea5ece909bc48a2023faf1
Mariam982440 <mariam1elhaimer@gmail.com>
2025-12-25 11:34:21 +0100

    feat: codage de la fonction authentifier de la classe user

--- classe/user.php
+++ classe/user.php
@@ -24,7 +24,34 @@ class User {
     public function getRole() { return $this->role; }
     public function getEmail() { return $this->email; }
 
-    
+    public static function authentifier($email, $password) {
+
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $stmt = $db->prepare("SELECT * FROM utilisateur WHERE email_usr = ?");
+        $stmt->execute([$email]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row && password_verify($password, $row['motdepasse_hash'])) {
+            
+            
+            
+            if ($row['role_usr'] === 'guide') {
+
+                if ($row['est_approuve'] == 0) return "NonApprouve";
+
+                return new Guide($row['id_usr'], $row['nom_usr'], $row['email_usr']);
+            } 
+            elseif ($row['role_usr'] === 'visiteur') {
+                return new Visiteur($row['id_usr'], $row['nom_usr'], $row['email_usr']);
+            }
+            elseif ($row['role_usr'] === 'admin') {
+                return new Admin($row['id_usr'], $row['nom_usr'], $row['email_usr']);
+            }
+        }
+        return false;
+    }
     public static function inscrire($nom, $email, $password, $role) {
         
         $database = new Database();

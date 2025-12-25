3d3fa60a4757284ec98fede6770354ee7d1c9143
Mariam982440 <mariam1elhaimer@gmail.com>
2025-12-25 11:33:19 +0100

    feat: codage de la fonction s'incrire de la classe user

--- classe/user.php
+++ classe/user.php
@@ -24,6 +24,41 @@ class User {
     public function getRole() { return $this->role; }
     public function getEmail() { return $this->email; }
 
-}  
+    
+    public static function inscrire($nom, $email, $password, $role) {
+        
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $check = $db->prepare("SELECT id_usr FROM utilisateur WHERE email_usr = ?");
+        $check->execute([$email]);
+
+        if ($check->rowCount() > 0) {
+            return "Cet email est déjà utilisé.";
+        }
+
+        // pour l'approbation guide = 0 autres = 1
+        // le role admin est interdit
+        if ($role == 'admin') {
+            return "Action interdite."; 
+        }
+           
+        $est_approuve = ($role == 'guide') ? 0 : 1;
+            
+        // hashage du mot de passe
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO utilisateur (nom_usr, email_usr, role_usr, motdepasse_hash, est_approuve) 
+                VALUES (?, ?, ?, ?, ?)";
+            
+        $stmt = $db->prepare($sql);
+            
+        if ($stmt->execute([$nom, $email, $role, $hash, $est_approuve])) {
+            return true; 
+        } else {
+            return "Erreur technique lors de l'enregistrement.";
+        }
+    }
+}
 
 ?>
